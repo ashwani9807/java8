0abdc26d665b1738b0deaf06dc6fae04f5f65f34
Ashwani Tripathi <Sachit@Ashwanis-MacBook-Pro.local>
2017-03-22 19:21:32 -0400

    Added functional Interface examples

---
+++ src/main/java/com/ashwani/poc/InterfacePOC.java
@@ -0,0 +1,39 @@
+package com.ashwani.poc;
+
+public class InterfacePOC implements DefaultInterface{
+
+	public static void main(String[] args) {
+		System.out.println("Calling static method");
+		DefaultInterface.showStaticDefaultMessage();
+		
+		System.out.println("Calling implemented method");
+		new InterfacePOC().showMessage();
+	}
+
+	@Override
+	public void showMessage() {
+		System.out.println("message from implemented method. Calling other overloaded methods");	
+		showDefaultMessage();
+		showAnotherDefaultMessage();
+	}
+}
+
+
+@FunctionalInterface
+interface DefaultInterface {
+	
+	public void showMessage();
+	
+	default void showDefaultMessage() {
+		System.out.println("inside showDefaultMessage");
+	}
+	
+	default void showAnotherDefaultMessage() {
+		System.out.println("inside showAnotherDefaultMessage");
+	}
+	
+	public static void showStaticDefaultMessage() {
+		System.out.println("inside showStaticDefaultMessage");
+	} 
+	
+}
